c31e03609e4a1747a0d6449e3cbf58a40b672451
eveykallmeyer <gkallmeyer@luc.edu>
2024-06-26 02:17:43 +0000

    A04

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,5 +4,17 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
+    public static void printOddCharacters(String string) {
+        for (int i=1; i < string.length(); i += 2) { // go through every other character in the string starting at the second letter
+            System.out.print(string.charAt(i) + " ");
+        }
+        System.out.println();
+    } // method printOddCharacters
 
+    public static void printEvenCharacters(String string) {
+        for (int i=0; i < string.length(); i += 2) { // go through every other character in the string starting at the first letter
+        System.out.print(string.charAt(i) + " ");
+        }
+        System.out.println();
+    } // method printEvenCharacters
 }
