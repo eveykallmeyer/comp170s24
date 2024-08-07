6b0adf5933f4dade606d15ec55714b1326f3cbf2
eveykallmeyer <gkallmeyer@luc.edu>
2024-08-07 15:41:36 +0000

    A10

--- Assignments/Assignment_10/FinalAssignment.java
+++ Assignments/Assignment_10/FinalAssignment.java
@@ -1,16 +1,20 @@
-public class FinalAssignment{
+import java.util.Arrays;
+
+public class FinalAssignment {
 
     // main method to create the arrays and a string for each problem, and to implement the merge, reverse, and simpleSort methods.
     public static void main(String[] args) {
 
         // Ages of my cousins on my mom's side
         int[] left = new int [] {11, 14, 16, 17};
+        int momsCousinsLength = left.length;
 
         // Ages of my cousins on my dad's side
         int[] right = new int [] {9, 10, 22, 25};
+        int dadsCousinsLength = right.length;
 
         // Merged array with all of my cousin's ages
-        int[] cousinsAges = merge(left, right);
+        int[] cousinsAges = merge(left, right, momsCousinsLength, dadsCousinsLength);
 
         // Test merge method
         System.out.println("My cousins on my mom's side are the following ages: " + Arrays.toString(left));
@@ -25,22 +29,26 @@ public class FinalAssignment{
 
     }
 
-    // merge method to return a single sorted array that was merged from merge two original arrays.
-    public static int[] merge(int[] left, int[] right){
+    // merge method to create a single sorted array that was merged from merge two original arrays.
+    public static int[] merge(int[] left, int[] right, int momsCousinsLength, int dadsCousinsLength) {
 
-        int[] cousinsAges = new int[left.length + right.length];
+        int[] cousinsAges = new int[momsCousinsLength + dadsCousinsLength];
         int i=0, j=0, k=0;
 
-        while (i < left.length) {
-            cousinsAges[k] = left[i];
-            i++;
-            k++;
+        while (i < momsCousinsLength && j < dadsCousinsLength) {
+            if (left[i] <= right[j]) {
+                cousinsAges[k++] = left[i++];
+            } else {
+                cousinsAges[k++] = right[j++];
+            }
         }
 
-        while (j < right.length) {
-            cousinsAges[k] = right[j];
-            j++;
-            k++;
+        while (i < momsCousinsLength) {
+            cousinsAges[k++] = left[i++];
+        }
+
+        while (j < dadsCousinsLength) {
+            cousinsAges[k++] = right[j++];
         }
 
         return cousinsAges;
