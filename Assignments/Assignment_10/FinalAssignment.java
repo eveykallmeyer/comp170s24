1899e43b2a40707e5c61ef7e54ff1421a870ad67
eveykallmeyer <gkallmeyer@luc.edu>
2024-08-07 16:18:46 +0000

    A10

--- Assignments/Assignment_10/FinalAssignment.java
+++ Assignments/Assignment_10/FinalAssignment.java
@@ -2,18 +2,21 @@ import java.util.Arrays;
 
 public class FinalAssignment {
 
-    // main method to create the arrays and a string for each problem, and to implement the merge, reverse, and simpleSort methods.
+
+    
+    // MAIN METHOD
+    //create the arrays and a string for each problem, and to implement the merge, reverse, and simpleSort methods.
     public static void main(String[] args) {
 
-        // Ages of my cousins on my mom's side
-        int[] left = new int [] {11, 14, 16, 17};
+        // Array #1 for merge method: Ages of my cousins on my mom's side
+        int[] left = new int [] {12, 17};
         int momsCousinsLength = left.length;
 
-        // Ages of my cousins on my dad's side
-        int[] right = new int [] {9, 10, 22, 25};
+        // Array #2 for merge method: Ages of my cousins on my dad's side
+        int[] right = new int [] {2, 9, 10, 22, 25};
         int dadsCousinsLength = right.length;
 
-        // Merged array with all of my cousin's ages
+        // Array #3 for merge method: Merged array with all of my cousin's ages
         int[] cousinsAges = merge(left, right, momsCousinsLength, dadsCousinsLength);
 
         // Test merge method
@@ -21,7 +24,13 @@ public class FinalAssignment {
         System.out.println("My cousins on my dad's side are the following ages: " + Arrays.toString(right));
         System.out.println("My cousins, in order from youngest to oldest, are the following ages:" + Arrays.toString(cousinsAges));
 
+
+        // String for reverse method
+        String reverseTestString = "lollapalooza";
+        String reverseTestResult = reverse(reverseTestString);
+
         // Test reverse method
+        System.out.println(reverseTestString + " spelled backwards is " + reverseTestResult);
 
 
         // Test simpleSort method
@@ -29,7 +38,10 @@ public class FinalAssignment {
 
     }
 
-    // merge method to create a single sorted array that was merged from merge two original arrays.
+
+
+    // MERGE METHOD
+    // create a single sorted array that was merged from merge two original arrays.
     public static int[] merge(int[] left, int[] right, int momsCousinsLength, int dadsCousinsLength) {
 
         int[] cousinsAges = new int[momsCousinsLength + dadsCousinsLength];
@@ -54,8 +66,28 @@ public class FinalAssignment {
         return cousinsAges;
     }
 
-    // reverse method to return the reverse of a string.
 
 
-    // simpleSort method to return an array in its ascending order
+    // REVERSE METHOD
+    // return the reverse of a string.
+    public static String reverse(String reverseTestString){
+
+        if (reverseTestString == null || reverseTestString.isEmpty()) {
+            return reverseTestString;
+        }
+
+        int reverseTestStringLength = reverseTestString.length();
+        char[] reverseTestChars = new char[reverseTestStringLength];
+
+        for (int i = 0; i < reverseTestStringLength; i++) {
+            char currentChar = reverseTestString.charAt(reverseTestStringLength - 1 - i);
+            reverseTestChars[i] = currentChar;
+        }
+        return new String(reverseTestChars);
+    }
+
+
+
+    // SIMPLESORT METHOD
+    // return an array in its ascending order
 }
